51f8099c407fabb127d6811536278411d2a02e42
thiagolinoz <thiagolinoz@gmail.com>
2025-12-20 10:15:59 -0300

    renomeia bucket no tf backend

--- infra/backend.tf
+++ infra/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "postech-fiap-fastfood-backend-eks-fase4"
+    bucket = "postech-fiap-fastfood-backend-eks-fase4-dynamo"
     key    = "backend/tfstate/terraform.tfstate"
     region = "us-east-1"
   }
