7913d62fb9a7f919c07fd988955adff394b2e497
thiagolinoz <thiagolinoz@gmail.com>
2025-12-22 08:59:31 -0300

    criar multiplos repos ecr

--- infra/ecr.tf
+++ infra/ecr.tf
@@ -1,4 +1,14 @@
+variable "repositorios" {
+  type    = set(string)
+  default = ["fastfood-pedidos", "fastfood-pessoas", "fastfood-produtos"]
+}
+
 resource "aws_ecr_repository" "postech_fastfood_repo" {
-  name                 = var.project_name
+  for_each             = var.repositorios
+  name                 = each.value
   image_tag_mutability = "MUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
 }
